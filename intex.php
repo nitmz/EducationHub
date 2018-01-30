e923c3cdd48738d863a5bbd844819a9b84e79af6
surajkumar787 <35957193+surajkumar787@users.noreply.github.com>
2018-01-30 18:00:53 +0530

    Create intex.php

---
+++ intex.php
@@ -0,0 +1,4 @@
+ecome better acquainted with GitHub through our bootcamp articles. See our GitHub flow for a process introduction. 
+Refer to our overview guides to walk through basic concepts.
+You can connect with developers around the world in the GitHub Community Forum to ask and answer questions, 
+learn, and interact directly with GitHub staff.
