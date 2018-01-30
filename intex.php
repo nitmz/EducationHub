9130e9b0b3bad92c8704c7c28df31fb2e26e6cdd
surajkumar787 <35957193+surajkumar787@users.noreply.github.com>
2018-01-30 18:06:05 +0530

    Update intex.php

--- intex.php
+++ intex.php
@@ -2,3 +2,6 @@ ecome better acquainted with GitHub through our bootcamp articles. See our GitHu
 Refer to our overview guides to walk through basic concepts.
 You can connect with developers around the world in the GitHub Community Forum to ask and answer questions, 
 learn, and interact directly with GitHub staff.
+add some more question here 
+
+
